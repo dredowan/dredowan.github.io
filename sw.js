24797223dbd06eba4258cca430fa190936c052c5
dredowan <66999345+dredowan@users.noreply.github.com>
2020-06-16 19:25:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,13 +14,12 @@ self.addEventListener('install', function(event) {
           '/manifest.json',
           '/index.html',
           '/js/index.js',
-          '/js/worker.js',
           '/css/index.css',
           '/offline/offline.html',
           'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
           'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
           'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
         ]);
       })
   )

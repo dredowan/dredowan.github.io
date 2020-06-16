d3cafa6b050e86c6901a8e49f00b154167fdb90b
dredowan <66999345+dredowan@users.noreply.github.com>
2020-06-16 19:32:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,7 +19,15 @@ self.addEventListener('install', function(event) {
           'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
           'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
           'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+          '/images/icons/icon-72x72.png',
+          '/images/icons/icon-96x96.png',
+          '/images/icons/icon-128x128.png',
+          '/images/icons/icon-144x144.png',
+          '/images/icons/icon-152x152.png',
+          '/images/icons/icon-192x192.png',
+          '/images/icons/icon-384x384.png',
+          '/images/icons/icon-512x512.png'
         ]);
       })
   )

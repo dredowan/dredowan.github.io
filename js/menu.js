5367800cfbd308dec29f66b95d807f7cf49c3a46
dredowan <66999345+dredowan@users.noreply.github.com>
2020-06-23 14:56:34 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,5 +5,6 @@ $(document).ready(function(){
         console.log(localStorage.playername);
     }else{
         console.log("USER NOT SIGNED");
+        window.location.href="index.html";
     }
 });
